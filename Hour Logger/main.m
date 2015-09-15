cb9136fa17a3b1a842730a018597a0937eb2078c
Matthew D'Amore <msd88@drexel.edu>
2015-09-15 18:04:36 -0400

    Initial Commit

---
+++ Hour Logger/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hour Logger
+//
+//  Created by Matthew D'Amore on 9/15/15.
+//  Copyright (c) 2015 Matthew Damore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
